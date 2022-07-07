dcb5aa9760771bc56c2735e350b28b9963b8a966
feriteja <feriteja@gmail.com>
2022-07-07 10:35:48 +0700

    assignment 2, input three times with validator

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 const validator = require("validator");
+const readline = require("readline");
 
-console.log(validator.isMobilePhone("0221394030058", "id-ID"));
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("What is yout name: ", (name) => {
+  rl.question("What is your contact: ", (number) => {
+    const isNumber = validator.isMobilePhone(number, "id-ID");
+    if (!isNumber) {
+      console.log(`number: ${number} is not valid`);
+      return rl.close();
+    }
+    rl.question("What is your email: ", (email) => {
+      const isEmail = validator.isEmail(email);
+      if (!isEmail) {
+        console.log(`email: ${email} is not valid`);
+        return rl.close();
+      }
+
+      console.log(
+        `\nYour name is ${name},\nwith number: ${number}, \nyour email address: ${email}`
+      );
+
+      rl.close();
+    });
+  });
+});
